161a25f0d8125e240d0255b7e405c8a1ce3f2066
YagoCardoso <y.maksuell@gmail.com>
2023-12-29 10:48:47 -0300

    Initial Commit

---
+++ FireworksNewYearSwifitUI/FireworksNewYearSwifitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireworksNewYearSwifitUIApp.swift
+//  FireworksNewYearSwifitUI
+//
+//  Created by User on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FireworksNewYearSwifitUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
